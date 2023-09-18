6eb0e2eed9c41ab6124209080b6479673ae1b117
charlie <charlie.nguyen.goldenowl@gmail.com>
2023-09-18 20:49:56 +0700

    init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const server = require('./server')
 
-const PORT = process.env.PORT || 3300
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`))
